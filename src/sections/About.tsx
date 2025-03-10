0d8f0b4ac5c18fd1d5fc7981857c8ba0dbef20b4
tim-chmieletzki <timchmieletzki@gmail.com>
2025-03-10 23:30:06 +0100

    Updated About

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,10 +3,6 @@ export default function About() {
         <section className="bg-purple-500 mx-auto mt-6 py-16 max-w-2xl">
             <div className="flex-col justify-start items-center bg-green-500">
                 <h1 className="bg-blue-500">Über mich</h1>
-                <div>2025</div>
-                <div>2024</div>
-                <div>2023</div>
-                <div>2022</div>
             </div>
         </section>
     );
